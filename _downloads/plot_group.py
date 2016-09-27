c66c75705df7f1683a938ce77802958e16385afe
Mainak Jas <mainak.jas@telecom-paristech.fr>
2016-09-27 11:44:18 +0200

    Make install

--- _downloads/plot_group.py
+++ _downloads/plot_group.py
@@ -9,10 +9,9 @@ import os.path as op
 
 import mne
 
-from library.config import meg_dir, subjects_dir, study_path
+from library.config import meg_dir, subjects_dir
 
-evokeds = mne.read_evokeds(op.join(study_path, 'MEG', 'grand_average-ave.fif'))
-evoked_famous, evoked_scrambled, evoked_unfamiliar = evokeds[:3]
+evokeds = mne.read_evokeds(op.join(meg_dir, 'grand_average-ave.fif'))[:3]
 
 ###############################################################################
 # Sensor-space. See :ref:`sphx_glr_auto_scripts_09-group_average_sensors.py`
@@ -20,7 +19,9 @@ evokeds[0].plot_joint(title='Famous')
 evokeds[1].plot_joint(title='Scrambled')
 evokeds[2].plot_joint(title='Unfamiliar')
 
-idx = evoked_famous.ch_names.index('EEG070')
+idx = evokeds[0].ch_names.index('EEG070')
+assert evokeds[1].ch_names[idx] == 'EEG070'
+assert evokeds[2].ch_names[idx] == 'EEG070'
 mapping = {'Famous': evokeds[0], 'Scrambled': evokeds[1],
            'Unfamiliar': evokeds[2]}
 mne.viz.plot_compare_evokeds(mapping, [idx], title='EEG070 (No baseline)')
@@ -37,4 +38,4 @@ stc = mne.read_source_estimate(fname, subject='fsaverage')
 
 brain = stc.plot(views=['ven'], hemi='both', subject='fsaverage',
                  subjects_dir=subjects_dir, initial_time=0.17, time_unit='s',
-                 clim={'lims':[99.75, 99.88, 99.98]})
+                 clim={'lims': [99.75, 99.88, 99.98]})
