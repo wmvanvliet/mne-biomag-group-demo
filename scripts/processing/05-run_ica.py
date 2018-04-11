3ea356e1775ba63746c4ff94dfdedde5f588f1e4
Marijn van Vliet <w.m.vanvliet@gmail.com>
2018-04-11 09:34:08 +0300

    Also fit ICA to EEG channels

--- scripts/processing/05-run_ica.py
+++ scripts/processing/05-run_ica.py
@@ -43,16 +43,10 @@ def run_ica(subject_id, tsss=None):
                            'run_concat-tsss_%d-ica.fif' % tsss)
     else:
         ica_name = op.join(meg_dir, subject, 'run_concat-ica.fif')
-    # Here we only compute ICA for MEG because we only eliminate ECG artifacts,
-    # which are not prevalent in EEG (blink artifacts are, but we will remove
-    # trials with blinks at the epoching stage).
     print('  Fitting ICA')
     ica = ICA(method='fastica', random_state=random_state,
               n_components=n_components)
-    picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=False,
-                           stim=False, exclude='bads')
-    ica.fit(raw, picks=picks, reject=dict(grad=4000e-13, mag=4e-12),
-            decim=11)
+    ica.fit(raw, reject=dict(grad=4000e-13, mag=4e-12), decim=11)
     print('  Fit %d components (explaining at least %0.1f%% of the variance)'
           % (ica.n_components_, 100 * n_components))
     ica.save(ica_name)
